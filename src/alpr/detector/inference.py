3718b8a0ee5d015b3469bc80fbed14e01d8c811e
luannt1010 <luanntai13@gmail.com>
2026-07-11 13:25:08 +0700

    add update yolo infer

--- src/alpr/detector/inference.py
+++ src/alpr/detector/inference.py
@@ -1,20 +1,21 @@
 import cv2
 import argparse
+from alpr.detector.helper_utils import read_img, draw_bbox, crop_image
+import numpy as np
 import torch
-from PIL import Image
 from torchvision import transforms
-from alpr.paths import DETECTOR_CHECKPOINT_DIR
-from alpr.detector.helper_utils import load_model_state_dict
+from alpr.paths import FASTRCNN_CHECKPOINT_DIR, YOLO_CHECKPOINT_DIR
+from alpr.detector.helper_utils import load_faster_rcnn_state_dict, load_yolo_state_dict
 from alpr.detector.config import MODEL_OBJECT
 
-class Detector:
+class FasterRCNNDetector:
     def __init__(self, model_obj, num_classes, weight_path):
-        self.model = load_model_state_dict(model_obj, num_classes, weight_path)
+        self.model = load_faster_rcnn_state_dict(model_obj, num_classes, weight_path)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     @torch.no_grad()
-    def detect(self, img_path):
-        orig_img = Image.open(img_path).convert("RGB")
+    def detect(self, img):
+        orig_img = read_img(img)
         trans = transforms.ToTensor()
         img = trans(orig_img)
         img = img.unsqueeze(0)
@@ -26,52 +27,68 @@ class Detector:
         scores, labels, boxes = predictions["scores"], predictions["labels"], predictions["boxes"]
         if 0 in (len(scores), len(labels), len(boxes)):
             return None
+        # Only get one license when vehicle go into parking area
         max_score_idx = torch.argmax(scores)
         score = scores[max_score_idx]
         label = labels[max_score_idx]
         bbox = boxes[max_score_idx].cpu().numpy().astype(int).tolist()
         return {"Score": score.item(), "Label": label.item(), "BBox": bbox}
 
-    def crop_image(self, img_path, bbox):
-        image = Image.open(img_path).convert("RGB")
-        return image.crop(bbox)
 
-    def draw_bbox(self, img_path, bbox, label, score):
-        orig_img = cv2.imread(img_path)
-        orig_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB)
-        x1, y1, x2, y2 = bbox
-        img = cv2.rectangle(orig_img, pt1=(x1,y1), pt2=(x2,y2), color=(0,255,0), thickness=3)
-        img = cv2.putText(img, f"{label, round(score,2)}", (x1, max(y1 - 10, 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
-        return img
+class YOLODetector:
+    def __init__(self, wp):
+        self.model = load_yolo_state_dict(wp)
 
+    def detect(self, img):
+        results = self.model(img)
+        outputs = []
+
+        for result in results:
+            boxes = result.boxes.xyxy.cpu().numpy()
+            scores = result.boxes.conf.cpu().numpy()
+            class_ids = result.boxes.cls.cpu().numpy()
+
+            outputs.append(boxes)
+            outputs.append(scores)
+            outputs.append(class_ids)
+
+        boxes, scores, class_ids = outputs
+        is_license_plate = class_ids == 2
+        max_score_idx = np.argmax(scores[is_license_plate])
+        final_boxes = boxes[max_score_idx].astype(int)
+        final_class_ids = class_ids[max_score_idx]
+        final_scores = scores[max_score_idx]
+        return {"Score": final_scores.item(), "Label": final_class_ids.astype(int).item(), "BBox": final_boxes.tolist()}
+
+class Detector:
+    def __init__(self, yolo=False):
+        if yolo:
+            self.detector = YOLODetector(YOLO_CHECKPOINT_DIR)
+        else:
+            self.detector = FasterRCNNDetector(MODEL_OBJECT["mobile_net_large"], 3, FASTRCNN_CHECKPOINT_DIR)
+    def detect(self, img):
+        return self.detector.detect(img)
 
 def get_args():
 
     parser = argparse.ArgumentParser()
-
-    parser.add_argument("--model", type=str, choices=list(MODEL_OBJECT.keys()), default="mobile_net_large")
-    parser.add_argument("--num_classes", type=int, default=3)
-    parser.add_argument("--weight_path", type=str, default=DETECTOR_CHECKPOINT_DIR)
+    parser.add_argument("--yolo", type=bool, default=False)
     parser.add_argument("--img_path", type=str, default="")
-
     return parser.parse_args()
 
 def main():
     args = get_args()
-    model_obj = args.model
-    num_classes = args.num_classes
-    weight_path = args.weight_path
+    is_yolo = args.yolo
     img_path = args.img_path
 
-    detector = Detector(MODEL_OBJECT[model_obj], num_classes, weight_path)
+    detector = Detector(is_yolo)
     results = detector.detect(img_path)
     if results == None:
         print("Not found license plate in the image.")
     else:
         print(results)
-        img_cropped = detector.crop_image(img_path, results["BBox"])
+        img_cropped = crop_image(img_path, results["BBox"])
         img_cropped.show()
 
 if __name__ == "__main__":
     main()
-
